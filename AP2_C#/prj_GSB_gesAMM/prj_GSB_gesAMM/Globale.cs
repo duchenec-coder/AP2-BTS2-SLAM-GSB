d4b28431446918fff503f72b2b9c6b958800dc1c
GuiGasp1477 <luffy272727@gmail.com>
2022-11-25 17:17:24 +0100

    dernieres modifs

--- AP2_C#/prj_GSB_gesAMM/prj_GSB_gesAMM/Globale.cs
+++ AP2_C#/prj_GSB_gesAMM/prj_GSB_gesAMM/Globale.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace prj_GSB_gesAMM
 {
-    internal class Class1
+    internal class Globale
     {
+        public static SqlConnection cnx;
+
     }
 }
